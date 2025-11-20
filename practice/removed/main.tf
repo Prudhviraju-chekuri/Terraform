6aeb4027598bda6db72ae9af1f8717f8b764516a
Prudhviraju-chekuri <ch.prudhviraju@gmail.com>
2025-11-20 14:41:20 +0530

    re imported the removed resource

--- practice/removed/main.tf
+++ practice/removed/main.tf
@@ -36,9 +36,20 @@ provider "aws" {
 } */
 
 # used removed block and destroy to false. the resource ec2 wont be managed by terraform, but wont destroy the real infrastructure.
-removed {
+/* removed {
   from = aws_instance.renamed_ec2
   lifecycle {
     destroy = false
+  }
+} */
+
+
+# imported the removed renamed_ec2 back to import_ec2 by adding the below resource.
+# but the tag name change will apply on the next terraform apply after terraform import.
+resource "aws_instance" "import_ec2" {
+    ami = "ami-0c398cb65a93047f2"
+  instance_type = "t3.micro" 
+  tags = {
+    Name = "import-ec2"
   }
 }
