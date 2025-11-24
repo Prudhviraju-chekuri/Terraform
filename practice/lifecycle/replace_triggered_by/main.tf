2a62d0f06f1df8596a9d71d6d2493f86ad1dcbe1
Prudhviraju-chekuri <ch.prudhviraju@gmail.com>
2025-11-24 20:33:22 +0530

    updated replace_triggered_by project

--- practice/lifecycle/replace_triggered_by/main.tf
+++ practice/lifecycle/replace_triggered_by/main.tf
@@ -15,17 +15,35 @@ variable "instance_type" {
   default = "t3.micro"
 }
 
-resource "aws_instance" "lifecycle_EC2" {
-  ami           = "ami-0c398cb65a93047f2"
-  instance_type = var.instance_type
-  key_name      = "mylinux"
+locals {
+  userdata_hash = filesha1("script.sh")
+}
 
+resource "null_resource" "userdata_trigger" {
+  triggers = {
+    hash = local.userdata_hash
+  }
+  
+}
+
+resource "random_id" "for-replace-instance-type" {
+  keepers = {
+    instance_type = var.instance_type
+  }
+  byte_length = 2
+}
+
+resource "aws_instance" "replace_triggered_ec2" {
+  ami = "ami-0c398cb65a93047f2"
+  instance_type = var.instance_type
+  key_name = "mylinux"
+  user_data = file("script.sh")
+  tags = {
+    Name = "rep_trigg_by_ec2"
+  }
   lifecycle {
     create_before_destroy = true
-    replace_triggered_by = [ md5(var.instance_type) ]
+    replace_triggered_by = [ random_id.for-replace-instance-type.hex, null_resource.userdata_trigger.id]
   }
-
-  tags = {
-    Name = "lifecycle_1"
-  }
+  
 }
