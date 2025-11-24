fa7595e7561d0865c78b8cd75bd96f6619d6b1bb
Prudhviraju-chekuri <ch.prudhviraju@gmail.com>
2025-11-24 13:40:42 +0530

    updated lifecycle projects

--- practice/lifecycle/ignore_changes/main.tf
+++ practice/lifecycle/ignore_changes/main.tf
@@ -11,3 +11,30 @@ provider "aws" {
   region = "us-east-1"
 }
 
+/* resource "aws_instance" "lifecycle_EC2" {
+  ami = "ami-0c398cb65a93047f2"
+  instance_type = "t3.micro"
+  key_name = "mylinux"
+  tags = {
+    Name = "Ignore_changes"
+    env = "Prod"
+  }
+} */
+
+resource "aws_instance" "lifecycle_EC2" {
+  ami = "ami-0c398cb65a93047f2"
+  instance_type = "t3.micro"
+  key_name = "mylinux"
+  tags = {
+    Name = "Ignore_changes"
+    env = "Prod"
+  }
+/*   lifecycle {
+    ignore_changes = [ tags ]
+  } */
+
+  lifecycle {
+    ignore_changes = [ tags["env"] ]
+  }
+}
+
--- practice/lifecycle/prevent_destroy/main.tf
+++ practice/lifecycle/prevent_destroy/main.tf
@@ -11,3 +11,25 @@ provider "aws" {
   region = "us-east-1"
 }
 
+/* resource "aws_s3_bucket" "prod-logs" {
+  bucket = "pchekuri-prod-logs"
+  
+} */
+
+resource "aws_s3_bucket" "prod-logs" {
+  bucket = "pchekuri-prod-logs"
+  lifecycle {
+    prevent_destroy = true
+  }
+  
+}
+
+# Error:
+/* Error: Instance cannot be destroyed
+│
+│   on main.tf line 19:
+│   19: resource "aws_s3_bucket" "prod-logs" {
+│
+│ Resource aws_s3_bucket.prod-logs has lifecycle.prevent_destroy set, but the plan calls for this resource to   
+│ be destroyed. To avoid this error and continue with the plan, either disable lifecycle.prevent_destroy or     
+│ reduce the scope of the plan using the -target option. */
