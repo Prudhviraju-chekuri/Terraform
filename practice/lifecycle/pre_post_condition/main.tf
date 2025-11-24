5489f4c5ccf3aa2ee8e7ab0d2f94fd4cb19e180e
Prudhviraju-chekuri <ch.prudhviraju@gmail.com>
2025-11-24 14:21:59 +0530

    added more lifecycle attribute projects

--- practice/lifecycle/pre_post_condition/main.tf
+++ practice/lifecycle/pre_post_condition/main.tf
@@ -11,3 +11,27 @@ provider "aws" {
   region = "us-east-1"
 }
 
+variable "instance_type" {
+  default = "t3.micro"
+}
+
+resource "aws_instance" "lifecycle_EC2" {
+  ami           = "ami-0c398cb65a93047f2"
+  instance_type = var.instance_type
+  key_name      = "mylinux"
+
+  lifecycle {
+    precondition {
+      condition = var.instance_type != "c7i-flex.large"
+      error_message = "flex large is too expensive"
+    }
+    postcondition {
+      condition = self.public_ip != ""
+      error_message = "EC2 did not receive a public IP!"
+    }
+  }
+
+  tags = {
+    Name = "lifecycle_1"
+  }
+}
--- practice/lifecycle/replace_triggered_by/main.tf
+++ practice/lifecycle/replace_triggered_by/main.tf
@@ -11,3 +11,21 @@ provider "aws" {
   region = "us-east-1"
 }
 
+variable "instance_type" {
+  default = "t3.micro"
+}
+
+resource "aws_instance" "lifecycle_EC2" {
+  ami           = "ami-0c398cb65a93047f2"
+  instance_type = var.instance_type
+  key_name      = "mylinux"
+
+  lifecycle {
+    create_before_destroy = true
+    replace_triggered_by = [ md5(var.instance_type) ]
+  }
+
+  tags = {
+    Name = "lifecycle_1"
+  }
+}
